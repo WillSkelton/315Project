bef4a123384315bbf0b79c8c74a810960a19bfd6
Will Skelton <william.f.skelton@gmail.com>
2022-04-17 18:31:35 -0700

    cranked it up to 11

--- main.py
+++ main.py
@@ -111,12 +111,12 @@ if __name__ == "__main__":
     stopList = parseStopList()
     comments, trainLabels = parseData("data/train.csv")
     vocabulary = generateVocabulary(comments, stopList)
-    trainData = generateDataFromComments(comments[:numberOfTrainingComments], vocabulary)
+    trainData = generateDataFromComments(comments, vocabulary)
 
     log(f"Number of Comments `{len(comments)}`")
     log(f"Number of Data Points `{len(trainData)}`")
 
-    dt = CreateAndTrainDecisionTree(trainData, trainLabels[:numberOfTrainingComments])
+    dt = CreateAndTrainDecisionTree(trainData, trainLabels)
 
     testComments = [
         "Yo bitch Ja Rule is more succesful then you'll ever be whats up with you and hating you sad mofuckas...i should bitch slap ur pethedic white faces and get you to kiss my ass you guys sicken me. Ja rule is about pride in da music man. dont diss that shit on him. and nothin is wrong bein like tupac he was a brother too...fuckin white boys get things right next time.,",
